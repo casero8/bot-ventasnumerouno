37a317b191ff01ea3310af6ec010adb287bd0a24
David Casero <davidcasero@MacBook-Air-de-David.local>
2026-06-15 14:39:47 +0200

    Evitar crashes (handlers globales) + visor de conversaciones por contacto en el panel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { startServer } from './server.js';
 
+// Que un error suelto NUNCA tire el proceso (evita reinicios que cortan conversaciones)
+process.on('unhandledRejection', (e) => console.error('[unhandledRejection]', e?.message || e));
+process.on('uncaughtException',  (e) => console.error('[uncaughtException]',  e?.message || e));
+
 startServer();
